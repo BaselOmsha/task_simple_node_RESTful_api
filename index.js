fff9fc6ecfbe8b756219f3173315a7c6be2f3539
Basel <basil.omsha@student.hamk.fi>
2023-04-18 20:34:05 +0300

    clean up

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const handlebars = require("handlebars");
 const app = express();
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 app.engine('handlebars', exphbs.engine({
@@ -15,27 +15,12 @@ app.engine('handlebars', exphbs.engine({
 
 app.set('view engine', 'handlebars');
 
-
-
 handlebars.registerHelper("eq", function (a, b) {
     return a === b;
 });
 
-// // CREATE A BOOK JSON
-// app.post('/api/newbook', async (req, res) => {
-// });
-
-// // GET ALL BOOKS JSON
-// app.get('/api/main', async (req, res) => {
-// });
-
-// // GET ONE BOOK JSON http://localhost:5000/2
-// app.get('/api/bookpage/:id', async (req, res) => {
-// });
-
 app.use('', require('./routes/rendered-books.js'));
 app.use('', require('./routes/api-books.js'));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
